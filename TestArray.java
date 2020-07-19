197301433fc5b229bd868a2f09a2f02c6400f33a
bhaskar.malireddy <bhaskar.malireddy@nbcuni.com>
2020-07-18 23:46:56 -0400

    adding new files

---
+++ TestArray.java
@@ -0,0 +1,13 @@
+public class TestArray {
+
+	public static void main(String[] args) {
+
+		for (int i=1; i<=10; i++) {
+	  
+	  System.out.println("Size of second Array ... "  + i);
+	}
+
+}
+	
+
+}
